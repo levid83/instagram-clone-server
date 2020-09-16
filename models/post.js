14ced7515913d169f8702a5c043bce8a8968b8b2
Levente Danel <danellevente@yahoo.com>
2020-09-16 12:53:38 +0300

    create mongoose models

---
+++ models/post.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+const { ObjectId } = Schema.Types;
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    likes: [{ type: ObjectId, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: ObjectId, ref: "User" },
+      },
+    ],
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Post", postSchema);
---
+++ models/user.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+const { ObjectId } = Schema.Types;
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetToken: String,
+  expireToken: Date,
+  pic: {
+    type: String,
+    default: "",
+  },
+  followers: [{ type: ObjectId, ref: "User" }],
+  following: [{ type: ObjectId, ref: "User" }],
+});
+
+export default model("User", userSchema);
